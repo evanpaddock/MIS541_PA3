c73237e364312cd523f1e6fa84f997ae92b19fd0
evanpaddock <edpaddock@crimson.ua.edu>
2023-11-01 23:56:24 -0500

    dfs created, idk how to merge

--- app.py
+++ app.py
@@ -1,119 +1,145 @@
 import re
 import pandas as pd
 import numpy as np
-from datetime import datetime
 import matplotlib.pyplot as plt
 
-cars_df = pd.read_csv("./cars_pa3.txt", sep="#")
-reviews_df = pd.read_csv("./reviews_pa3.txt", sep="\t")
+
+# !Functions
+def get_month_year_wordcount_cols(dataframe: pd.DataFrame):
+    dataframe["year"] = dataframe.apply(
+        lambda row: pd.to_datetime(row["date"]).year, axis=1
+    )
+    dataframe["month"] = dataframe.apply(
+        lambda row: pd.to_datetime(row["date"]).month, axis=1
+    )
+    dataframe["word_count"] = dataframe.apply(
+        lambda row: 0
+        if pd.isna(row["comment"])
+        else (len(re.findall(r"[\w']+", row["comment"]))),
+        axis=1,
+    )
+    return dataframe
 
 
-def extract_year(date_str):
-    date_object = datetime.strptime(date_str, "%m-%d-%Y")
-    return date_object.year
+def get_count_rating_by_year(year: pd.DataFrame):
+    count_year_df = (
+        reviews_df[reviews_df["year"] == year]["rating"]
+        .value_counts()
+        .fillna(0)
+        .astype(int)
+    )
+    return count_year_df
 
 
-def extract_month(date_str):
-    date_object = datetime.strptime(date_str, "%m-%d-%Y")
-    return date_object.month
+def merge_dfs(left_df: pd.DataFrame, right_df: pd.DataFrame):
+    merged_df = (
+        pd.merge(left_df, right_df, on="rating", how="outer").fillna(0).astype(int)
+    )
+    return merged_df
 
 
-def extract_word_count(comment):
-    if pd.isna(comment):
-        return 0
-    return len(re.findall(r"[\w']+", comment))
+def get_ticks(dataframe: pd.DataFrame, step: float, min_0: bool = True):
+    if min_0:
+        min = 0
+    else:
+        min = dataframe.values.min()
+    max = dataframe.values.max()
+    return np.arange(min, max + (step * 2), step)
 
 
-# Create the columns year, month, word_count by extracting values using the functions above
-reviews_df["year"] = reviews_df["date"].apply(extract_year)
-reviews_df["month"] = reviews_df["date"].apply(extract_month)
-reviews_df["word_count"] = reviews_df["comment"].apply(extract_word_count)
+def get_column(
+    list_of_items: list, list_of_items_to_add: list, add_space: bool = False
+):
+    combo_items = []
+    for item in list_of_items:
+        for i in list_of_items_to_add:
+            if add_space:
+                combo_items.append(f"{item} {i}")
+            else:
+                combo_items.append(f"{item}{i}")
 
-# ! Descriptive Statistics
-print("Average Rating: ")
+    return combo_items
+
+
+# !Read in files to dfs
+
+reviews_df = pd.read_csv("reviews_pa3.txt", sep="\t", index_col="serial_no")
+cars_df = pd.read_csv("cars_pa3.txt", sep="#")
+
+
+# !ADDING COLUMNS
+
+reviews_df = get_month_year_wordcount_cols(reviews_df)
+print(reviews_df.to_string(), end="\n\n")
+
+# !DESCRIPTIVE STATISTICS
+print("Average Rating:")
 print(reviews_df["rating"].describe(), end="\n\n")
 
-print("Average Word Count: ")
-print(reviews_df["word_count"].describe())
+print("Average Word Count:")
+print(reviews_df["word_count"].describe(), end="\n\n")
+
 
 # !RATINGS COUNT DISTRIBUTION BY YEAR
-# Create three DataFrames for 2019, 2020, and 2021
-df_2019 = reviews_df[reviews_df["year"] == 2019]
-df_2020 = reviews_df[reviews_df["year"] == 2020]
-df_2021 = reviews_df[reviews_df["year"] == 2021]
 
-# Count the rating values for each year
-count_2019 = df_2019["rating"].value_counts().fillna(0).astype(int)
-count_2020 = df_2020["rating"].value_counts().fillna(0).astype(int)
-count_2021 = df_2021["rating"].value_counts().fillna(0).astype(int)
+count_2019 = get_count_rating_by_year(2019)
+count_2020 = get_count_rating_by_year(2020)
+count_2021 = get_count_rating_by_year(2021)
 
-# Merge the three DataFrames on the rating column
-df_2019_20_21_rating_years = (
-    pd.concat(
-        [count_2019, count_2020, count_2021],
-        axis=1,
-        keys=["count_2019", "count_2020", "count_2021"],
-    )
-    .fillna(0)
-    .sort_index()
-)
+df_2019_2020_rating_counts = merge_dfs(count_2019, count_2020)
+df_2019_2020_2021_rating_counts = merge_dfs(df_2019_2020_rating_counts, count_2021)
 
-df_2019_20_21_rating_years.plot(kind="bar")
+df_2019_2020_2021_rating_counts = df_2019_2020_2021_rating_counts.sort_index()
+
+df_2019_2020_2021_rating_counts.columns = ["count_2019", "count_2020", "count_2021"]
+
+df_2019_2020_2021_rating_counts.plot(kind="bar")
+
+y_ticks = get_ticks(df_2019_2020_2021_rating_counts, 1)
+
 plt.title("Number of Ratings by Year")
 plt.xlabel("Rating")
-plt.xticks([0, 1.0, 2.0, 3.0, 4.0, 5.0])
-plt.ylabel("Counts")
-plt.yticks([0, 1, 2, 3, 4, 5])
-
-# !AVERAGE WORD COUNT BY YEAR
-avgword_count_by_year = reviews_df.groupby("year")["word_count"].mean()
-avgword_count_by_year = avgword_count_by_year.to_frame()
-avgword_count_by_year.columns = ["avg_word_count"]
-
-min = 0.0
-max = 5.5
-step = 0.5
-y_ticks = np.arange(min, max + step, step)
-
-avgword_count_by_year = avgword_count_by_year.sort_index()
-
-avgword_count_by_year.plot(kind="bar", legend=True)
-plt.title("Average Word Count by Year")
-plt.xlabel("Year")
-plt.ylabel("Word Count")
-plt.xticks(rotation=360)
+plt.ylabel("Count")
 plt.yticks(y_ticks)
 
+print(df_2019_2020_2021_rating_counts.to_string(), end="\n\n")
+
+# !AVERAGE WORD COUNT BY YEAR
+avg_word_count_year_df = pd.DataFrame(reviews_df.groupby(["year"])["word_count"].mean())
+avg_word_count_year_df.columns = ["avg_word_count"]
+
+print(avg_word_count_year_df, end="\n\n")
+
+y_ticks = get_ticks(avg_word_count_year_df, 0.5)
+
+avg_word_count_year_df.plot(kind="bar")
+plt.legend()
+plt.title("Average Word Count by Year")
+plt.xlabel("Year")
+plt.xticks(rotation=360)
+plt.ylabel("Word Count")
+plt.yticks(y_ticks)
+
+
 # !RATINGS COUNT DISTRIBUTION BY SEASONS AND YEARS
+
+seasonname_year = get_column(["Winter", "Summer", "Fall"], [19, 20, 21], add_space=True)
+year_season = get_column(["2019", "2020", "2021"], [1, 2, 3])
+
+rating_count_by_season_year_df = pd.DataFrame(
+    {
+        "seasonname_year": seasonname_year,
+        "year_season": year_season,
+    }
+).set_index("seasonname_year")
+
 reviews_df = reviews_df.dropna(subset=["rating", "comment"], how="any")
 reviews_df = reviews_df[reviews_df["word_count"] != 0]
 
-seasonname_year_df = reviews_df.sort_values(["year", "month"])
-
-def get_season_name(row):
-    seasons = {
-        1: "Winter",
-        2: "Winter",
-        3: "Winter",
-        4: "Winter",
-        5: "Summer",
-        6: "Summer",
-        7: "Summer",
-        8: "Summer",
-        9: "Fall",
-        10: "Fall",
-        11: "Fall",
-        12: "Fall",
-    }
-
-    month = seasons[row["month"]]
-    year = str(row["year"])
-    
-    return {'seasonname_year': f"{month} {year[2:]}", 'year_season': f"{year}{row['month']}"}
+year_month_rating_counts = pd.DataFrame(
+    reviews_df.groupby(["year", "month"])["rating"].value_counts()
+)
 
 
-final_df = seasonname_year_df.apply(get_season_name, axis=1)
-final_df = pd.DataFrame(final_df.tolist()).set_index(['seasonname_year'])
-count_df = final_df.groupby('seasonname_year')['year_season'].value_counts()
-
-print(final_df.head(10))
+print(year_month_rating_counts)
+print(rating_count_by_season_year_df)
